105c24ebe3b409e6743a87383bfac9b4bb0c5bbc
PranathiBadugu12 <45870618+PranathiBadugu12@users.noreply.github.com>
2020-11-20 00:19:41 +0800

    Add files via upload

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -1,6 +1,42 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 export default class LoginComponent extends Component {
-  
+    constructor(props){
+        super(props)
+        this.state = {
+            users: [],
+            userId : ""
+        }
+        this.updateInput = this.updateInput.bind(this);
+        this.validate = this.validate.bind(this);
+    }
+    componentDidMount() {
+        axios.get(`http://localhost:3000/Sheet1`)
+          .then(res => {
+            const userss = res.data;
+            this.setState({ users : userss});
+          })
+      }
+      
+      updateInput(event){
+        this.setState({userId : event.target.value})
+        }
+    validate(){
+       var temp = this.state.userId
+       var i = 0
+        for(i = 0; i < this.state.users.length; i++) {
+            var obj = this.state.users[i];
+            if(temp == obj.CustomerId)
+            {
+                alert("Valid User")
+                break
+            }
+        }
+        if(i >= this.state.users.length){
+        alert("Invalid Users")
+        }
+        
+    }
    
    render() {
       return (
@@ -9,14 +45,10 @@ export default class LoginComponent extends Component {
                 <h3>Sign In</h3>
 
                 <div className="form-group">
-                    <label>Email address</label>
-                    <input id="email" type="email" className="form-control" placeholder="Enter email" />
+                    <label>User Id </label>
+                    <input onChange={this.updateInput} type="text" className="form-control" placeholder="Enter User Id" required/>
                 </div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input id="password" type="password" className="form-control" placeholder="Enter password" />
-                </div>
 
                 <div className="form-group">
                     <div className="custom-control custom-checkbox">
@@ -25,7 +57,7 @@ export default class LoginComponent extends Component {
                     </div>
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <button type="button" className="btn btn-primary btn-block" onClick={this.validate}>Submit</button>
                 
             </form>
          </div>
